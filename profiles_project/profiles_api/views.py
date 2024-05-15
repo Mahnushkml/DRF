7c72945c08e1006cd6fe8ac0e7f1ecbe50ddcb5d
Mahnush <mahnushkamal@gmail.com>
2024-05-15 11:48:19 +0330

    adding a get method

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
+
+class Hello_API(APIView):
+    def get(self,request):
+        return Response({"test":True}, status=status.HTTP_200_OK)
