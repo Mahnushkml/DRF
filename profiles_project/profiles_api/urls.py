1b6756990d5ebb9b35a83eda468b7444a591f56c
Mahnush <mahnushkamal@gmail.com>
2024-05-18 08:59:28 +0330

    added viewset to profiles api

--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import Hello_API
+from django.urls import path, include
+# from .views import Hello_API
+
+from rest_framework.routers import DefaultRouter
+from profiles_api import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 urlpatterns=[
-    path('hello-api/', Hello_API.as_view()),
+    path('hello-api/', views.Hello_API.as_view()),
+    path('', include(router.urls))
 ]
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from profiles_api.serializer import HelloSerializer
+
 # Create your views here.
 
 class Hello_API(APIView):
@@ -38,5 +40,47 @@ class Hello_API(APIView):
         """Delete an object"""
         return Response({'method': 'DELETE'})
 
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    def list(self, request):
+        "return a hello message "
+        a_viewset = [
+            'uses action (list, ctrate, retrieve, update, partial_update)',
+            'Automatically maps to  URLs using Routers',
+            'Provides more functionality with less code',
+            ]
+        return Response({'message':'Hello', 'a_viewset':a_viewset})
+    
+    def create(self, request):
+        """Create a new Hello message"""
+        ser2 = HelloSerializer(data=request.data)
+
+        if ser2.is_valid():
+            name = ser2.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response ({'message': message})
+        else:
+            return Response(
+                ser2.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method':'GET'})
+    
+    def update(self, request, pk=None):
+        """Handle upddating an object"""
+        return Response({'http_method':'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """Handle upddating part of an object"""
+        return Response({'http_method':'PATCH'})
+    
+    def defstroy(self, request, pk=None):
+        """Handle removing of an object"""
+        return Response({'http_method':'DELETE'})
 
 
